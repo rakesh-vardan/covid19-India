0ab98fa1f0c5e4f8a04672680624fef275b8bf0f
Rakesh Vardan <rakeshlearnit@gmail.com>
2020-10-17 10:27:13 +0530

    first commit

---
+++ src/test/java/com/jaguars/Sample.java
@@ -0,0 +1,12 @@
+package com.jaguars;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Sample {
+
+    @Test
+    public void testMe() {
+        Assert.assertEquals(1, 1);
+    }
+}
